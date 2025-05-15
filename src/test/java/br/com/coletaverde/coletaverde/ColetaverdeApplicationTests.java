c6b202cde8c0a220721e56554e1450d8e9d2f276
Andesson Reis <andessonreys@gmail.com>
2025-05-15 00:58:24 -0300

    ⚙️ chore(spring): initialize core Spring project setup

---
+++ src/test/java/br/com/coletaverde/coletaverde/ColetaverdeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coletaverde.coletaverde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColetaverdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
