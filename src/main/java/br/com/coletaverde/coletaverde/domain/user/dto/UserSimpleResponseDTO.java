768298b0b1ee84ad0f0d5912673f920cdbc0a797
Andesson Reis <andessonreys@gmail.com>
2025-05-15 13:49:42 -0300

    ✨ feat(user): add DTOs for user creation, login, and simplified response

---
+++ src/main/java/br/com/coletaverde/coletaverde/domain/user/dto/UserSimpleResponseDTO.java
@@ -0,0 +1,15 @@
+package br.com.coletaverde.coletaverde.domain.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSimpleResponseDTO {
+
+    @JsonProperty("email")
+    private String email;
+}
